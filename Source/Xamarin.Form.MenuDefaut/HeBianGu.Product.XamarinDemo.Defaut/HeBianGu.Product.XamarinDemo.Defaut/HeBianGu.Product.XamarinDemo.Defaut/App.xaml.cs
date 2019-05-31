412f382816ff0b6ec1739b7dd7a361dda2675434
HeBianGu <908293466@qq.com>
2019-05-31 17:56:00 +0800

    1111

---
+++ Source/Xamarin.Form.MenuDefaut/HeBianGu.Product.XamarinDemo.Defaut/HeBianGu.Product.XamarinDemo.Defaut/HeBianGu.Product.XamarinDemo.Defaut/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HeBianGu.Product.XamarinDemo.Defaut.Services;
+using HeBianGu.Product.XamarinDemo.Defaut.Views;
+
+namespace HeBianGu.Product.XamarinDemo.Defaut
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Source/Xamarin.Form.MenuDefaut/HeBianGu.Product.XamarinDemo.Defaut/HeBianGu.Product.XamarinDemo.Defaut/HeBianGu.Product.XamarinDemo.Defaut/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HeBianGu.Product.XamarinDemo.Defaut.Models;
+
+namespace HeBianGu.Product.XamarinDemo.Defaut.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
